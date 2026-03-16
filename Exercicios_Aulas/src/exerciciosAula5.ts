5e90d929c206a6039d5425173cf4ef6d1b7ab84e
Lucas Rafael Bersot Linares <lucasblinares@gmail.com>
2026-03-16 15:55:29 -0300

    Adicionando os Exercicios 1 ao 4 da Aula 5

--- Exercicios_Aulas/src/exerciciosAula5.ts
+++ Exercicios_Aulas/src/exerciciosAula5.ts
@@ -1,7 +1,41 @@
 //Exercício 1:
-
 function deixarMaiusculos(palavra:string):string{
     return palavra.toUpperCase();
 } 
+const deixarMaiusculosLambda = (palavra: string): string => palavra.toUpperCase();
 
+console.log(deixarMaiusculosLambda("lucas"))
 console.log(deixarMaiusculos("lucas"));
+
+//Exercicio 2:
+function elevarNumero(a:number, b:number):number {
+    return a**b;
+}
+const elevarNumeroLambda = (a: number, b: number): number => a**b;
+
+console.log(elevarNumero(2,3));
+console.log(elevarNumeroLambda(5,2));
+
+//Exercicio 3:
+
+function dobrarArray(a:number[]):number[] {
+    for (let i = 0; i < a.length; i++) {
+        a[i] = Number(a[i])* 2;
+    }       
+    return a;
+}
+
+const dobrarArrayLambda = (a2: number[]): number[] => {
+    for (let ia = 0; ia < a2.length; ia++) {
+        a2[ia] = Number(a2[ia]) * 2;
+    }
+    return a2;
+};  
+
+console.log(dobrarArray([1,2,3]));
+console.log(dobrarArrayLambda([4,5,6]));
+
+//Exercicio 4:
+
+
+
